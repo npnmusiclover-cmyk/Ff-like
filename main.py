55935c70eb81cdf4c475cb26a1d2d6a9b41643cd
npnmusiclover-cmyk <npnmusiclover@gmail.com>
2026-05-29 19:41:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = 8351165824
 
 # FULL API
-API_URL = "https://aniketbramha.om-divine.workers.dev/key="
+API_URL = "https://aniketbramha.om-divine.workers.dev/?key=lundlo&num="
 
-API_KEY = "lundlo&num="
+API_KEY = "lundlo"
 
 # CHANNELS
 CHANNEL_1_ID = "@joinforfree110"
